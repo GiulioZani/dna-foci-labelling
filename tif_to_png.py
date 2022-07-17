8167499d165d74ea6f347ce365830394f7d47d72
GiulioZani <giulio.zani@gmail.com>
2022-07-17 18:33:16 +0200

    update

--- tif_to_png.py
+++ tif_to_png.py
@@ -5,14 +5,16 @@ import ipdb
 
 def main(src: str, dist: str):
     if not os.path.exists(dist):
+        print('creating output directory')
         os.mkdir(dist)
     file_names = [file_name for file_name in os.listdir(src)]
     for file_name in file_names:
-        print(file_name)
-        img = imread(os.path.join(src, file_name))
-        dest_file_name = os.path.join(dist, file_name[:-3] + "png")
-        print(f"{dest_file_name=}")
-        cv.imwrite(dest_file_name, img)
+        if file_name.endswith('.TIF'):
+            print(file_name)
+            img = imread(os.path.join(src, file_name))
+            dest_file_name = os.path.join(dist, file_name[:-3] + "png")
+            print(f"{dest_file_name=}")
+            cv.imwrite(dest_file_name, img)
         # img = cv.imread(dest_file_name, cv.IMREAD_UNCHANGED)
         # ipdb.set_trace()
 
